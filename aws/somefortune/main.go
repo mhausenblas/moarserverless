9c86c18d1163acd7e8d5841b4b93ad0ee9b865e4
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-12-08 12:12:03 +0000

    inits AWS Lambda implementation

---
+++ aws/somefortune/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func serverError(err error) (events.APIGatewayProxyResponse, error) {
+	fmt.Println(err.Error())
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusInternalServerError,
+		Headers: map[string]string{
+			"Access-Control-Allow-Origin": "*",
+		},
+		Body: fmt.Sprintf("%v", err.Error()),
+	}, nil
+}
+
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmessage := "TBD"
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Headers: map[string]string{
+			"Content-Type":                "application/json",
+			"Access-Control-Allow-Origin": "*",
+		},
+		Body: string(fmessage),
+	}, nil
+
+}
+
+func main() {
+	lambda.Start(handler)
+}

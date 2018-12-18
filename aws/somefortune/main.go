fdc58b16748053d14e9b076a5a80a21ef7cd95d9
Michael Hausenblas <michael.hausenblas@gmail.com>
2018-12-18 11:18:26 +0000

    implements random msg in Lambda

--- aws/somefortune/main.go
+++ aws/somefortune/main.go
@@ -1,26 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"math/rand"
 	"net/http"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func serverError(err error) (events.APIGatewayProxyResponse, error) {
-	fmt.Println(err.Error())
-	return events.APIGatewayProxyResponse{
-		StatusCode: http.StatusInternalServerError,
-		Headers: map[string]string{
-			"Access-Control-Allow-Origin": "*",
-		},
-		Body: fmt.Sprintf("%v", err.Error()),
-	}, nil
-}
-
 func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	fmessage := "TBD"
+	// see https://github.com/bmc/fortunes/
+	messages := []string{"It's bad luck to be superstitious.", "A city is a large community where people are lonesome together.", "A critic is a legless man who teaches running.", "Dobbin's Law: When in doubt, use a bigger hammer."}
+	rand.Seed(time.Now().Unix())
+	fmessage := messages[rand.Intn(len(messages))]
 	return events.APIGatewayProxyResponse{
 		StatusCode: http.StatusOK,
 		Headers: map[string]string{
